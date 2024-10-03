e139572a70041b765884ee1d111a40d31a38e808
Nicolas Viéville <nicolas.vieville@uphf.fr>
2024-10-03 14:35:02 +0200

    RPMFUSION: Fix for 'index 0 is out of range for type 'uvm_gpu_chunk_t *[*]' and uvm_page_directory_t *[*]' kernel 6.8.x traces. Suggested by: https://forums.developer.nvidia.com/t/\ ubsan-array-index-out-of-bounds-complaints-in-newer-kernels/271705/5 Thanks to Bruce Jerrick https://bugzilla.rpmfusion.org/show_bug.cgi?id=7069
    Signed-off-by: Nicolas Viéville <nicolas.vieville@uphf.fr>

--- kernel/nvidia-uvm/uvm8_mmu.h
+++ kernel/nvidia-uvm/uvm8_mmu.h
@@ -76,7 +76,7 @@ struct uvm_page_directory_struct
 
     // pointers to child directories on the host.
     // this array is variable length, so it needs to be last to allow it to take up extra space
-    uvm_page_directory_t *entries[0];
+    uvm_page_directory_t *entries[];
 };
 
 struct uvm_mmu_mode_hal_struct
